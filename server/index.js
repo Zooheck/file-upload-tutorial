77621b2c49a3ebd54bde2dc27545f60ba8b8f927
Zooheck <davidscottflack@gmail.com>
2019-02-26 17:56:12 -0600

    Add file recepticle and create react app

--- server/index.js
+++ server/index.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const helmet = require('helmet')
 const cors = require('cors')
+const upload = require('./upload');
 
 const server = express()
 
+var corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200
+  };
+
 server.use(helmet())
-server.use(cors())
+server.use(cors(corsOptions))
+
+server.post('/upload', upload)
 
 server.listen(8000, () => {
     console.log('Server listening on port 8000.');
