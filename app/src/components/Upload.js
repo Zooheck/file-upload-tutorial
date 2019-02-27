6b4ff9bbc37c67c21850b28654a2f86bfd20dbe5
Zooheck <davidscottflack@gmail.com>
2019-02-26 19:14:20 -0600

    Start progress bar

--- app/src/components/Upload.js
+++ app/src/components/Upload.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react'
 import './upload.css'
+
+import Dropzone from './Dropzone'
+import Progress from './Progress'
 export default class upload extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      files: [],
+      uploading: false,
+      uploadProgress: {},
+      successfullUploaded: false
+    }
+  }
+  onFilesAdded = files => {
+    this.setState(prevState => ({
+      files: prevState.files.concat(files)
+    }))
   }
   render() {
     return (
@@ -10,10 +24,20 @@ export default class upload extends Component {
         <span className="Title">Upload Files</span>
         <div className="Content">
           <div>
-
+            <Dropzone
+              onFilesAdded={this.onFilesAdded}
+              disabled={this.state.uploading || this.state.successfullUploaded}
+            />
           </div>
           <div className="Files">
-          
+          {this.state.files.map(file => {
+              return (
+                <div key={file.name} className="Row">
+                  <span className="Filename">{file.name}</span>
+                  {this.renderProgress(file)}
+                </div>
+              );
+            })}
           </div>
         </div>
         <div className="Actions">
